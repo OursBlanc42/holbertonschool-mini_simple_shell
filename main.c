a0fd945da16f6da751452b2e4abde76f1cac5b86
OursBlanc42 <simon.regnier@holbertonstudents.com>
2024-12-17 15:32:37 +0100

    add prompt invit function

--- main.c
+++ main.c
@@ -2,18 +2,31 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
-/**
- * main - Entry point
- * Description : mini simple shell
- * Return: Always 0.
- */
 
 int main(void)
 {
+	/* declare & initialize variable */
+	char *string = NULL;
+	size_t buffer_size = 0;
+	ssize_t readed;
+
+play_again:
+
 	/* Waiting for input */
 	printf("(づ｡◕‿◕｡)づ ♥ ");
 
+	/* Read the input and print the input */
+	readed = getline(&string, &buffer_size, stdin);
+
+	if (readed == -1)
+		printf("error\n");
+	else
+		printf("%s", string);
+
+	goto play_again;
 
 }
 
