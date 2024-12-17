f1030c695d237d75844ff22ad38d0d6111f3e058
OursBlanc42 <simon.regnier@holbertonstudents.com>
2024-12-17 16:35:19 +0100

    super simple shell implemented read and execute call
    Co-authored-by: Tefa95100 <Tefa95100@users.noreply.github.com>

--- main.c
+++ main.c
@@ -6,27 +6,75 @@
 #include <sys/wait.h>
 
 
+/**
+ * main - Entry point
+ * Description : Mini Simple Shell
+ * Return: Nothing (waiting to ctrl + c (abort))
+ */
+
 int main(void)
 {
 	/* declare & initialize variable */
 	char *string = NULL;
 	size_t buffer_size = 0;
 	ssize_t readed;
+	int i = 0;
+	
+	pid_t child_pid;
 
-play_again:
+
+play_again: 
 
 	/* Waiting for input */
-	printf("(づ｡◕‿◕｡)づ ♥ ");
+	printf("(づ｡◕‿‿◕｡)づ ♥ ");
 
-	/* Read the input and print the input */
+	/* Read the input and check if succeed */
 	readed = getline(&string, &buffer_size, stdin);
 
 	if (readed == -1)
-		printf("error\n");
+		{
+			printf("(╯°□°）╯︵ ┻━┻ (Error getline)\n");
+			goto play_again;
+		}
+
+	/* fork current process and check if succeed */
+	child_pid = fork();
+
+	if (child_pid == -1)
+	{
+		printf("(╯°□°）╯︵ ┻━┻ (Error fork)\n");
+		goto play_again;
+	}
+
+
+
+	/* clean string (remove last \n char) */
+	for(i=0; string[i] != '\0'; i++)
+	{
+		if(string[i] == '\n')
+		{
+			string[i] = '\0';
+		}
+	}
+
+	/* setup argv[] for execve */
+	char *argv[] = {string, NULL };
+
+	/* execute child process */
+	if (child_pid == 0)
+	{
+		/* Check and execute command if no error */
+		if (execve(argv[0], argv, NULL) == -1)
+		{
+			printf("(╯°□°）╯︵ ┻━┻ (Error execve)\n");
+			goto play_again;
+		}
+	}
+
+	/* waiting for child finish execution */
 	else
-		printf("%s", string);
-
-	goto play_again;
-
+	{
+			wait(NULL);
+			goto play_again;
+	}
 }
-
