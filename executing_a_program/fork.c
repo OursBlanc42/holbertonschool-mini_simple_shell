186017a7a6974642e43c0f0ba96b798037377105
OursBlanc42 <simon.regnier@holbertonstudents.com>
2024-12-17 14:49:10 +0100

    add forkwaitexecve.c task

--- executing_a_program/fork.c
+++ executing_a_program/fork.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <unistd.h>
-
+#include <stdlib.h>
+#include <sys/types.h>
 /**
  * main - fork example
  *
--- executing_a_program/forkwaitexecve.c
+++ executing_a_program/forkwaitexecve.c
@@ -2,10 +2,12 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 /**
  * main - Entry point
- * Description : Execute the command ls -l /tmp in 5 differents child processes.
+ * Description : Execute the command ls -l /tmp in 5 differents child processes
  * Each child should be created by the same process (the father)
  * Wait for a child to exit before creating a new child.
  * Return: Always 0.
@@ -13,7 +15,56 @@
 
 int main(void)
 {
+	/* declare and initialize variable */
+	pid_t child_pid[] = {0, 0, 0, 0, 0};	/* declare array to store PID */
+	pid_t father_pid;
+	pid_t current_pid;
+	int index = 0;
 
+	printf("Main program started\n");
+
+	father_pid = getpid();
+
+	/* setup argv[] for execve */
+	char *argv[] = {"/bin/ls", "-l", "/tmp/test", NULL };
+
+	/* loop 5x */
+	for (index = 0; index < 5; index++)
+	{
+		/* fork process and check if suceed */
+		child_pid[index] = fork();
+
+		if (child_pid[index] == -1)
+		{
+			printf("Error!!\n");
+			return (1);
+		}
+
+		if (child_pid[index] == 0) /* mean = we are in the "child process" */
+		{
+			current_pid = getpid();
+			printf("I am the child !\n");
+			printf("Current child pid = \t\t%d\n", current_pid);
+
+			if (execve(argv[0], argv, NULL) == -1)
+			{
+				printf("Error!!\n");
+				return (1);
+			}
+
+		}
+		else
+		{
+			wait(NULL);
+			printf("I am you're father !\n");
+			printf("father_pid = \t\t%d\n", father_pid);
+			printf("child_pid[index] = \t%d\n\n", child_pid[index]);
+		}
+
+		printf("######## NEXT ########\n");
+	}
+
+	return (1);
 
 }
 
