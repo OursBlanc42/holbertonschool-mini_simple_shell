ea797d6597401d1649b52b38bdb252614a088134
OursBlanc42 <simon.regnier@holbertonstudents.com>
2024-12-17 10:30:43 +0100

    add dir exec a program task

---
+++ executing_a_program/forkwaitexecve.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * main - Entry point
+ * Description : Execute the command ls -l /tmp in 5 differents child processes.
+ * Each child should be created by the same process (the father)
+ * Wait for a child to exit before creating a new child.
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+
+
+}
+
