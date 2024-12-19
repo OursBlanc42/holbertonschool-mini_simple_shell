cb269ee6ff185368ca429a216d489d2978116d77
Tefa95100 <damien.julien.95100@gmail.com>
2024-12-19 23:34:23 +0100

    Add comment, change research by strstr instead use strchr for search separator and compare with strncmp

--- _which/_getenv.c
+++ _which/_getenv.c
@@ -9,13 +9,22 @@
  */
 char *_getenv(const char *search)
 {
-	while (*environ != NULL)
+	char **ptr_env = environ;
+	char *sep_equal = NULL;
+
+	/*browse the environnement with a pointer and search = in the current string*/
+	while (*ptr_env != NULL)
 	{
-		if (strstr(*environ, search) != NULL)
-		{
-			return (*environ);
-		}
-		environ++;
+		sep_equal = strchr(*ptr_env, '=');
+
+		/**
+		* If the separator if find and  the name of variable correspond
+		* return the string after the separator
+		*/
+		if (sep_equal != NULL && strncmp(*ptr_env, search, strlen(search)) == 0)
+			return (sep_equal + 1);
+
+		ptr_env++;
 	}
-	return (*environ);
+	return (NULL);
 }
