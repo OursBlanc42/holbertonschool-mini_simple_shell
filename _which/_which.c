9ce049390b72e37db282ce8bbd69b404b03c4012
OursBlanc42 <simon.regnier@holbertonstudents.com>
2024-12-20 09:23:33 +0100

    add argc argv to drive function

--- _which/_which.c
+++ _which/_which.c
@@ -7,29 +7,47 @@
 #include "find.h"
 
 /**
- * _which - Rewrite of which
- * Description : to retrieve the location of executable in current PATH
- * Return: NULL or executable path
+ * _which - Rewrite of the `which` command
+ * Description: This program searches for the location of an executable
+ *				in the directories specified by the PATH environment variable.
+ * 
+ * Algorithm:
+ * 1. Split the PATH environment variable into a list of directories.
+ * 2. Loop through each directory:
+ *    - Check if the command exists and is executable.
+ * 3. Return the full path of the command if found; print an error otherwise.
+ *
+ * Return: 0 if the command is found, 1 otherwise.
  */
 
-int main(void)
+
+int main(int argc, char *argv[])
 {
 	/* declare & initialize variable */
-	char *name = "PATH";			/* name of var env */
-	char *mysearch = "/neofetch";
+	char *name = "PATH"; /* name of var env */
+	char *mysearch = NULL;
 	char *separator = ":";			/* separator	*/
-	char *env_path = _getenv(name);	/* get the env path */
+	char *env_path = getenv(name);	/* get the env path */
 	list_t *path_list;				/* single linked list */
 	struct stat buffer_stat;	/* buffer to store stat result */
 	char *string_concat = NULL;
 
+	if (argc != 2)
+	{
+		printf("Please enter one command name after which\n");
+		return (1);
+	}
+
+	mysearch = argv[1];
+
+	/* generate path list and stock in single linked list */
 	path_list = chopper(env_path, separator);
 
 	/* Loop through the linked list */
 	while (path_list != NULL)
 	{
+		string_concat = strcat(path_list->str, "/");
 		string_concat = strcat(path_list->str, mysearch);
-		printf("%s", path_list->str);
 
 		/* check if stat return value, that mean the thing exist */
 		if (stat(path_list->str, &buffer_stat) == 0)
